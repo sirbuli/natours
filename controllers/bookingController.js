6daa521e3ed495cf2e834bb0e51a04b8a15ed1eb
sirbuli <bulibulislaw@gmail.com>
2023-06-15 12:33:55 +0200

    Added async await to webhookcheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,7 +64,7 @@ const createBookingCheckout = async (session) => {
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
@@ -78,7 +78,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
   res.status(200).json({ received: true });
 };
 
